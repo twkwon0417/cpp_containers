54462a0146f51b213da6ebf76d0b24f400295bc2
twkwon0417 <kwontony123@konkuk.ac.kr>
2025-10-30 15:31:14 +0900

    feat: implement is_integral

--- type_traits.h
+++ type_traits.h
@@ -16,5 +16,66 @@ namespace ft{
     struct enable_if<true, T> {
         typedef T type;
     };
+
+    // is_integral
+    template<typename T>
+    struct is_integral {
+        static const bool value = false;
+    };
+
+    template <>
+    struct is_integral<bool> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<char> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<signed char> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<unsigned char> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<wchar_t> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<short> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<unsigned short> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<int> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<unsigned int> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<long> {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_integral<unsigned long> {
+        static const bool value = true;
+    };
 }
 #endif //CPP_CONTAINERS_TYPE_TRAITS_H
