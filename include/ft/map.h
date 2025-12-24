0c524bc67788b4b26715752a838372b98ebfa4e5
twkwon0417 <kwontony123@konkuk.ac.kr>
2025-12-24 15:58:25 +0900

    implement map

--- include/ft/map.h
+++ include/ft/map.h
@@ -1,189 +1,242 @@
 #ifndef FT_MAP_H
 #define FT_MAP_H
 
+#include "pair.h"
 #include <cstddef>
 #include <functional>
-#include "pair.h"
 
-namespace ft
-{
+namespace ft {
 
-    template <class Key, class T, class Compare = std::less<Key> >
-    class map
-    {
-      public:
-        // ---------------- Member types ----------------
-        typedef Key                    key_type;
-        typedef T                      mapped_type;
-        typedef ft::pair<const Key, T> value_type;
-        typedef Compare                key_compare;
-        typedef std::size_t            size_type;
+template <class Key, class T, class Compare = std::less<Key>> class map {
+public:
+  // ---------------- Member types ----------------
+  typedef Key key_type;
+  typedef T mapped_type;
+  typedef ft::pair<const Key, T> value_type;
+  typedef Compare key_compare;
+  typedef std::size_t size_type;
 
-      private:
-        // ---------------- Node definition ----------------
-        struct Node
-        {
-            value_type value;
-            Node      *parent;
-            Node      *left;
-            Node      *right;
-            bool       red;
+private:
+  // ---------------- Node definition ----------------
+  struct Node {
+    value_type value;
+    Node *parent;
+    Node *left;
+    Node *right;
+    bool red;
 
-            Node(const value_type &v) : value(v), parent(nullptr), left(nullptr), right(nullptr), red(true) {}
+    Node(const value_type &v)
+        : value(v), parent(nullptr), left(nullptr), right(nullptr), red(true) {}
+  };
 
-			bool operator<(const Node& other) {
-				return value.first() < other.value.first();
-			}
-        };
+  Node *_root;
+  size_type _size;
+  key_compare _comp;
 
-        Node root = Node();  // Map의 생성자에서 처리해줄꺼야
+public:
+  // ---------------- Constructor ----------------
+  map() : _root(nullptr), _size(0), _comp(key_compare()) {}
 
-        Node find(key_type key_to_be_inserted, Node node) {  // 일단 진짜 찾는건 나중에 생각해, 예외처리 나중에
-            key_type key = node.value.fisrt;
+  // ---------------- Capacity ----------------
+  size_type size() const { return _size; }
+  bool empty() const { return _size == 0; }
 
-            if(key > key_to_be_inserted) {
-                if(node.left != nullptr) {
-                    return find(node.left);
-                }
-            }
-            else if(key < key_to_be_inserted) {
-                if(node.right != nullptr) {
-                    return find(node.right);
-                }
-            }
-            // key == key_to_be_inserted 이거나 node가 들어갈 위치가 nullptr 즉 들어가야하는 위치인 경우
-            return node;
+  // ---------------- Modifiers ----------------
+  // BST 삽입 + RBT 균형 복구
+  ft::pair<Node *, bool> insert(const value_type &pair_to_be_inserted) {
+    Node *new_node = new Node(pair_to_be_inserted);
+
+    // Case 1: 빈 트리인 경우
+    if (_root == nullptr) {
+      _root = new_node;
+      _root->red = false; // 루트는 항상 black
+      ++_size;
+      return ft::make_pair(_root, true);
+    }
+
+    // BST 삽입 위치 찾기
+    Node *parent = _find_insert_position(pair_to_be_inserted.first, _root);
+
+    // 이미 같은 키가 존재하는 경우
+    if (!_comp(parent->value.first, pair_to_be_inserted.first) &&
+        !_comp(pair_to_be_inserted.first, parent->value.first)) {
+      delete new_node; // 중복 키는 삽입하지 않음
+      return ft::make_pair(parent, false);
+    }
+
+    // 새 노드를 부모에 연결
+    new_node->parent = parent;
+    if (_comp(pair_to_be_inserted.first, parent->value.first)) {
+      parent->left = new_node;
+    } else {
+      parent->right = new_node;
+    }
+    ++_size;
+
+    // RBT 속성 복구 (fix-up)
+    _insert_fixup(new_node);
+
+    return ft::make_pair(new_node, true);
+  }
+
+  void erase() {
+    // TODO: implement
+  }
+
+private:
+  // ---------------- Helper functions ----------------
+
+  // 삽입 위치의 부모 노드를 찾는 함수
+  Node *_find_insert_position(const key_type &key, Node *node) {
+    if (_comp(key, node->value.first)) {
+      // key < node의 key -> 왼쪽으로
+      if (node->left != nullptr) {
+        return _find_insert_position(key, node->left);
+      }
+      return node; // 삽입 위치의 부모
+    } else if (_comp(node->value.first, key)) {
+      // key > node의 key -> 오른쪽으로
+      if (node->right != nullptr) {
+        return _find_insert_position(key, node->right);
+      }
+      return node; // 삽입 위치의 부모
+    }
+    // key == node의 key (이미 존재)
+    return node;
+  }
+
+  // RBT 삽입 후 속성 복구
+  void _insert_fixup(Node *node) {
+    while (node != _root && node->parent->red) {
+      Node *parent = node->parent;
+      Node *grandparent = parent->parent;
+
+      if (grandparent == nullptr)
+        break;
+
+      if (parent == grandparent->left) {
+        // Case A: 부모가 조부모의 왼쪽 자식
+        Node *uncle = grandparent->right;
+
+        if (uncle != nullptr && uncle->red) {
+          // Case 1: Uncle이 RED -> Recoloring
+          parent->red = false;
+          uncle->red = false;
+          grandparent->red = true;
+          node = grandparent; // 위로 올라가며 계속 검사
+        } else {
+          // Case 2 & 3: Uncle이 BLACK (또는 nullptr)
+          if (node == parent->right) {
+            // Case 2: node가 오른쪽 자식 -> Left Rotate로 Case 3으로 변환
+            node = parent;
+            _rotate_left(node);
+            parent = node->parent;
+          }
+          // Case 3: node가 왼쪽 자식 -> Right Rotate
+          parent->red = false;
+          grandparent->red = true;
+          _rotate_right(grandparent);
         }
+      } else {
+        // Case B: 부모가 조부모의 오른쪽 자식 (대칭)
+        Node *uncle = grandparent->left;
 
-        void insert(value_type pair_to_be_inserted) {
-            Node parent = find(pair_to_be_inserted.first);  // parent라는 변수명이 삽입되는 애 기준임
-            Node node_to_be_inserted = Node(pair_to_be_inserted);
-
-            if(parent.first > pair_to_be_inserted.first) {
-                parent.left = node_to_be_inserted;
-            }
-            else if(parent.first < pair_to_be_inserted.first) {
-                parent.right = node_to_be_inserted;
-            }
-            // TODO: 같은(equal) 경우는 예외 처리 해줘야해 일단 넘기겠습니다.
-
-            // TODO: 규칙을 위배하는 친구들 찾고 해결하기
-
-
+        if (uncle != nullptr && uncle->red) {
+          // Case 1: Uncle이 RED -> Recoloring
+          parent->red = false;
+          uncle->red = false;
+          grandparent->red = true;
+          node = grandparent;
+        } else {
+          // Case 2 & 3: Uncle이 BLACK
+          if (node == parent->left) {
+            // Case 2: node가 왼쪽 자식 -> Right Rotate로 Case 3으로 변환
+            node = parent;
+            _rotate_right(node);
+            parent = node->parent;
+          }
+          // Case 3: node가 오른쪽 자식 -> Left Rotate
+          parent->red = false;
+          grandparent->red = true;
+          _rotate_left(grandparent);
         }
+      }
+    }
+    // 루트는 항상 BLACK
+    _root->red = false;
+  }
 
-        void erase() {
+  // Uncle 노드 찾기
+  Node *_find_uncle(Node *new_node) {
+    Node *parent_node = new_node->parent;
+    if (parent_node == nullptr)
+      return nullptr;
 
-        }
+    Node *grand_node = parent_node->parent;
+    if (grand_node == nullptr)
+      return nullptr;
 
-        void search(value_type to_find) {   // TODO: yet implemented
-            // Node temp_node = Node(temp);
-            Node* node = &temp_node;
-        }
+    if (grand_node->left == parent_node) {
+      return grand_node->right;
+    }
+    return grand_node->left;
+  }
 
-        bool check_reconstruct(Node* new_node) {
-            Node* uncle_node = new_node->parent->parent->right;
-
-            if(new_node->parent->red && !uncle_node->red) {
-                return true;
-            }
-            return false;
-        }
-
-        bool check_recoloring(Node* new_node) {
-            Node* uncle_node = new_node->parent->parent->right;
-
-            if(new_node->parent->red && uncle_node->red) {
-                return true;
-            }
-            return false;
-        }
-
-        // node를 이동시키면, parent, left, right 다 update 시켜줘야해
-        // reconstruct의 경우 부모 노드를 검정색으로 바꿔서 문제 없음
-        // Uncle node가 가장 큼으로 오른쪽 아래 -> 오른쪽 아래로 가게 됨 따라서 자식 노드를 빨강으로 바꿔도 아무 문제가 생기지 않음
-        // => 연쇄적으로 재구성(?)들이 발생하지 않아. + 재구성이 일어나는 상황은 RBT의 모든 리프 노드에서 Black Depth는 같다. 에 의해 생기지 않아.
-        // rotate로 subtree가 이동해도 문제가 생기지 않아 -> 왜나면 uncle이 무조건 black인 경우에만 발생하니까
-		void reconstruct(Node* new_node) {
-
-		}
-
-        //   (x)                       (y)
-        //   / \     Left Rotate(x)    / \
+  //   (x)                       (y)
+  //   / \     Left Rotate(x)    / \
         //  α  (y)   ------------->  (x)  γ
-        //     / \                   / \
+  //     / \                   / \
         //    β   γ  <------------- α   β
-        //           Right Rotate(y)
+  //           Right Rotate(y)
 
-        // nullptr로 만들어줘야 할 애들은 없어 보임
-        void _rotate_left(Node* x) {
-            Node* a = x->left;
-            Node* y = x->right;
-            Node* c = y->right;
-            // 아래 코드는 nullptr일수도 있어
-            Node* b = y->left;
+  void _rotate_left(Node *x) {
+    Node *y = x->right;
 
-            Node* parent = x->parent;
-            y->parent = parent;
-            if (parent != nullptr) {
-                // x가 parent의 오른이면 오른, 왼이면 왼으로 update해줘야 해
-                if (parent->left == x) {
-                    parent->left = y;
-                }
-                else {
-                    parent->right = y;
-                }
-            }
-            else {
-                this->root = y;
-            }
+    // β를 x의 오른쪽 자식으로
+    x->right = y->left;
+    if (y->left != nullptr) {
+      y->left->parent = x;
+    }
 
-            x->left = a;
-            x->right = b;
+    // y의 부모를 x의 부모로
+    y->parent = x->parent;
+    if (x->parent == nullptr) {
+      _root = y;
+    } else if (x == x->parent->left) {
+      x->parent->left = y;
+    } else {
+      x->parent->right = y;
+    }
 
-            y->left = x;
-            y->right = c;
-            if (b != nullptr) {
-                b->parent = x;
-            }
-            x->parent = y;
-        }
+    // x를 y의 왼쪽 자식으로
+    y->left = x;
+    x->parent = y;
+  }
 
-        void _rotate_right(Node* x) {
-            Node* a = x->right;
-            Node* y = x->left;
-            Node* c = y->left;
-            // 아래 코드는 nullptr일수도 있어
-            Node* b = y->right; // left -> right
+  void _rotate_right(Node *x) {
+    Node *y = x->left;
 
-            Node* parent = x->parent;
-            y->parent = parent;
+    // β를 x의 왼쪽 자식으로
+    x->left = y->right;
+    if (y->right != nullptr) {
+      y->right->parent = x;
+    }
 
-            if (parent != nullptr) {
-                // x가 parent의 오른이면 오른, 왼이면 왼으로 update
-                if (parent->left == x) {
-                    parent->left = y;
-                }
-                else {
-                    parent->right = y;
-                }
-            }
-            else {
-                this->root = y;
-            }
+    // y의 부모를 x의 부모로
+    y->parent = x->parent;
+    if (x->parent == nullptr) {
+      _root = y;
+    } else if (x == x->parent->right) {
+      x->parent->right = y;
+    } else {
+      x->parent->left = y;
+    }
 
-            x->right = a;
-            x->left = b;
-
-            y->right = x;
-            y->left = c;
-
-            if (b != nullptr) {
-                b->parent = x;
-            }
-            x->parent = y;
-        }
-    };
+    // x를 y의 오른쪽 자식으로
+    y->right = x;
+    x->parent = y;
+  }
+};
 
 } // namespace ft
 
